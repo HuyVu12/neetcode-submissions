ef836ee69e306a457c67ecc6f339f370ef0c5cbb
HuyVu12 <64604579+HuyVu12@users.noreply.github.com>
2026-09-14 15:59:50 +0700

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int d[45+1] = {0};
+        d[0] = 1;
+        d[1] = 1;
+        for(int i = 2; i <= n; ++i) d[i] = d[i-1] + d[i-2];
+        return d[n];
+    }
+};
